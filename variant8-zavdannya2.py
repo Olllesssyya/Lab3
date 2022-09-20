c6dfcc00f7f93f1f7c60883c4636609e67c454fb
Olesia Fesiuk <O.Fesyuk_FIT_9_22_B_d@knute.edu.ua>
2022-09-20 21:16:18 +0300

    Додали посилання на GitHub

--- variant8-zavdannya2.py
+++ variant8-zavdannya2.py
@@ -1,5 +1,6 @@
 #Лабароторна 3 варіант 8 завдання1. Посилання на GitHub https://github.com/Olllesssyya/Lab3
 import sys
-n = int(sys.argv[1]) #Задаємо кількість кутів привильного n-кутника
+import argparse
+n=int(sys.argv[1])
 a=(n-2)/n*180 #Знаходимо величину кута правильного n-кутника
 print("Величину кута правильного n-кутника:", a)
